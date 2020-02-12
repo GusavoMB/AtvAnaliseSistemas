00470b7663e1057c4f627656aad6bd4de5f56137
Juniorlpes <bento.lopes50@gmail.com>
2020-02-11 22:18:29 -0300

    create sql

---
+++ sql/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `id12555854_hospitalar`.`hospital` ( `cpf` VARCHAR(11) NOT NULL , `nome` VARCHAR(50) NOT NULL , `idade` INT(3) UNSIGNED NOT NULL , `telefone` INT(11) NOT NULL , `descricao` VARCHAR(150) NULL , PRIMARY KEY (`cpf`(11))) ENGINE = InnoDB;
